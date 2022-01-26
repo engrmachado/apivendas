8d8abfa1a49a586c0e7f07e6b3a564abf0d9465a
Rogerio <engrmachado@gmail.com>
2022-01-25 21:24:00 -0300

    k

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = Router();
 
 routes.get('/', (request, response) => {
-  return response.json({ message: 'usuário acessando a paginasss' });
+  return response.json({ message: 'usuário acessando a paginassss' });
 });
 
 export default routes;
