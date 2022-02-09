b7e91ffcf12354b2eccca2ef6a9b8a37151194fe
Rogerio <engrmachado@gmail.com>
2022-02-09 19:40:14 -0300

    aula

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,9 +2,11 @@ import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import {errors} from 'celebrate'
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
+import { celebrate } from 'celebrate';
 
 
 const app = express();
@@ -12,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
